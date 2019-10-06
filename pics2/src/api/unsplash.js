5919be468eeeede55f76d7e38ff0baa64dda92c9
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-06 14:36:03 +0800

    redo project - pics V1

---
+++ pics2/src/api/unsplash.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'https://api.unsplash.com/',
+    headers: {
+        Authorization: 'Client-ID 6e746e45df17a1605b9f6587c22eeaeca1d27273e1ff649408ab10f9d43deee0'
+    }
+});
