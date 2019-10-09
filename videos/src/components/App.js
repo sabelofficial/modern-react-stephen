a6b32e7566aee701dab253a908aa8e34297d7eff
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-09 21:26:02 +0800

    11th section - video app finished

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,21 +1,46 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    onTermSubmit = (term) => {
-        console.log(youtube)
-        youtube.get('/search', {
+    state = { videos: [], selectedVideo: null };
+
+    componentDidMount() {
+        this.onTermSubmit('skincare');
+    }
+
+    onTermSubmit = async (term) => {
+        const res = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+        this.setState({ 
+            videos: res.data.items,
+            selectedVideo: res.data.items[0]
+        });
+    };
+
+    onVideoSelect = (video) => {
+        this.setState({ selectedVideo: video });
     };
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar onTermSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
