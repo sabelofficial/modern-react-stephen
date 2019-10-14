3b73c7c9fe3a97e3edc6625759ebc9ccecc77bca
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-14 16:26:01 +0800

    integrating react with redux - songs project done!

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import SongList from './SongList';
+import SongDetail from './SongDetail';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="ui container grid" style={{marginTop: '10px'}}>
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
+        </div>
     );
 }
 
