d50c97068e854d2335f7b89bd56368aa9a9da03b
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-15 20:53:27 +0800

    15th section - blogs project done

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,19 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // _.chain(getState().posts)
+    //     .map('userId')
+    //     .uniq()
+    //     .forEach(id => dispatch(fetchUser(id)))
+    //     .value();
+
+};
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
@@ -9,7 +24,7 @@ export const fetchPosts = () => async (dispatch) => {
     });
 };
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ 
@@ -17,3 +32,15 @@ export const fetchUser = (id) => async (dispatch) => {
         payload: response.data 
     });
 };
+
+// export const fetchUser = id => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({ 
+//         type: 'FETCH_USER', 
+//         payload: response.data 
+//     });
+// });
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
+// import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component {
-    componentDidMount() {
-        this.props.fetchUser(this.props.userId);
-    }
+    // componentDidMount() {
+    //     this.props.fetchUser(this.props.userId);
+    // }
 
     render() {
+        const { user } = this.props;
+
+        if(!user) {
+            return null;
+        }
+
         return (
-            <div>User Header</div>
+            <div className="header">{user.name}</div>
         );
     }
 }
 
-export default connect(null, { fetchUser })(UserHeader);
+const mapStateToProps = (state, ownProps) => {
+    return { user : state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps/*, { fetchUser }*/)(UserHeader);
