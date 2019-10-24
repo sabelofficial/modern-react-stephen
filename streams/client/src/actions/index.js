214a84199968cc06bf4d5d60bb1ccb7ac51b3227
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-24 23:58:30 +0800

    create browser history object from programmatic navigation, refactor streams list page

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
     SIGN_IN,
     SIGN_OUT,
@@ -22,9 +23,12 @@ export const signOut = () => {
     };
 };
 
-export const createStream = (formValues) => async (dispatch) => {
-    const res = await streams.post('/streams', formValues);
+export const createStream = (formValues) => async (dispatch, getState) => {
+    const { userId } = getState().auth;
+    const res = await streams.post('/streams', { ...formValues, userId });
+    
     dispatch({ type: CREATE_STREAM, payload: res.data });
+    history.push('/');
 };
 
 export const fetchStreams = () => async (dispatch) => {
