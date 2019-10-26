b1c8cf1aa7f043cfb25948becd1ae9b0d1eba2c6
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-26 21:46:45 +0800

    finished setting up translate project, implemented React Context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language });
+    };
+
     render() {
         return (
-            <div className="ui container">App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')} />
+                    <i className="flag ph" onClick={() => this.onLanguageChange('filipino')} />
+                </div>
+
+                <ColorContext.Provider value="red">
+                    <LanguageContext.Provider value={this.state.language}>
+                        <UserCreate />
+                    </LanguageContext.Provider>      
+                </ColorContext.Provider>             
+            </div>
         );
     }
 }
---
+++ translate/src/components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+    renderSubmit(value) {
+        return value === 'english' ? 'Submit' : 'Ipasa';
+    }
+
+    render() {
+        return (
+            <ColorContext.Consumer>
+                {(color) => 
+                    <button className={`ui button ${color}`}>
+                        <LanguageContext.Consumer>
+                            {(value) => this.renderSubmit(value)}
+                        </LanguageContext.Consumer>
+                    </button>
+                }
+                
+            </ColorContext.Consumer>            
+        );
+    }
+}
+
+export default Button;
