574981ea88785b689e5bb8ec683a8f4b84ae158a
Maria Jezebel Jimenez <mmjimenez1@up.edu.ph>
2019-10-27 20:50:48 +0800

    custom hooks

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,28 +1,16 @@
 import React from 'react';
-import axios from 'axios';
+import useResources from './useResources';
 
-class ResourceList extends React.Component {
-    state = { resources: [] };
+const ResourceList = ({ resource }) => {
+    const resources = useResources(resource);
 
-    async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    
-        this.setState({ resources: response.data });
-    }
-
-    async componentDidUpdate(prevProps) {
-        if(prevProps.resource !== this.props.resource) {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
-    
-            this.setState({ resources: response.data });
-        }
-    }
-
-    render() {
-        return (
-            <div>{this.state.resources.length}</div>
-        );
-    }
+    return (
+        <ul>
+            {resources.map(record => (
+                <li key={record.id}>{record.title}</li>
+            ))}
+        </ul>
+    );
 }
 
 export default ResourceList;
